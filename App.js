43251ad7ca71bbc432420f67d4e0f9228d0c3731
ronen.goren <ronen.goren@gmail.com>
2018-10-26 14:50:30 -0400

    airmap

--- App.js
+++ App.js
@@ -1,120 +1,76 @@
-
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Dimensions } from 'react-native';
-import PropTypes from 'prop-types'; // ES6
-
-const { width, height } = Dimensions.get('window')
-const SCREEN_HEIGHT = height
-const SCREEN_WIDTH = width
-const ASPECT_RATIO = width / height
-const LATITUDE_DELTA = 0.0922
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
-
+import { AppRegistry, StyleSheet, View, Dimensions } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+let { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE = 0;
+const LONGITUDE = 0;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 export default class App extends Component {
-  constructor(props) {
-    super(props)
+  constructor() {
+    super();
     this.state = {
-      initialPosition: {
-        latitude: 0,
-        longitude: 0,
-        latitudeDelta: 0,
-        longitudeDelta: 0,
-      },
-      markerPosition: {
-        latitude: 0,
-        longitude: 0
-      }
-    }
-  }
-
-  watchID: ?number = null;
-
-
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition((position) => {
-      var lat = parseFloat(position.coords.latitude)
-      var long = parseFloat(position.coords.longitude)
-      var initialRegion = {
-        latitude: lat,
-        longitude: long,
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
         latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA
-      }
-      this.setState({ initialPosition: initialRegion })
-      this.setState({ markerPosition: initialRegion })
-
-    }, (error) => alert(JSON.stringify(error)),
-      { enableHighAccuracy: true, timeout: 2000, maximumAge: 1000 })
-    this.watchID = navigator.geolocation.watchPosition((position) => {
-      var lat = parseFloat(position.coords.latitude)
-      var long = parseFloat(position.coords.longitude)
-
-      var lastRegion = {
-        latitude: lat,
-        longitude: long,
         longitudeDelta: LONGITUDE_DELTA,
-        latitudeDelta: LATITUDE_DELTA
       }
-      this.setState({ initialPosition: lastRegion })
-      this.setState({ markerPosition: lastRegion })
-    })
+    };
   }
-
-
-  componentWillMount() {
-    navigator.geolocation.clearWatch(this.watchID)
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }
+        });
+      },
+      (error) => console.log(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+    this.watchID = navigator.geolocation.watchPosition(
+      position => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }
+        });
+      }
+    );
   }
-
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <MapView
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          style={styles.map}
-          region={this.state.initialPosition}
-        >
-          <MapView.Marker
-            coordinate={this.state.markerPosition} >
-            <View style={styles.radius}>
-              <View style={styles.marker}></View>
-            </View>
-          </MapView.Marker>
-        </MapView>
-      </View>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.container}
+        showsUserLocation={true}
+        region={this.state.region}
+        onRegionChange={region => this.setState({ region })}
+        onRegionChangeComplete={region => this.setState({ region })}
+      >
+
+      </MapView>
     );
   }
 }
-
 const styles = StyleSheet.create({
-  radius: {
-    height: 50,
-    width: 50,
-    borderRadius: 50 / 2,
-    overflow: 'hidden',
-    backgroundColor: 'rgba(0, 122, 255, 0.1)',
-    borderWidth: 1,
-    borderColor: 'rgba(0, 112, 255, 0.3)',
-    alignItems: 'center',
-    justifyContent: "center"
-  },
-  marker: {
-    height: 20,
-    width: 20,
-    borderWidth: 3,
-    borderColor: 'white',
-    borderRadius: 20 / 2,
-    overflow: 'hidden',
-    backgroundColor: '#007AFF'
-  },
   container: {
-    ...StyleSheet.absoluteFillObject,
-    // height: 400,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
+    height: '100%',
+    width: '100%',
+  }
 });
